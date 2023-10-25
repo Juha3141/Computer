875b2f3538840f9265544a8ee3cf32645e0ab870
Ian Juha Cho <ianisnumber2027@gmail.com>
2023-10-25 16:17:54 +0900

    Update fibonacci.asm

--- interpreter/fibonacci.asm
+++ interpreter/fibonacci.asm
@@ -1,8 +1,13 @@
 ; just basic fibonacci program
-MOV R0 , 0x00
-MOV R1 , 0x01
-MOV R2 , 0x00
-L1:
+; set port
+LDR($PORTA) 0x00
+LDR($PORTRW) 0b0001 ; using port 1 as write port
+LDR($PORTE)  0b0001 ; enable port 1
+FIBONACCI:
+    MOV R0 , 0x00
+    MOV R1 , 0x01
+    MOV R2 , 0x00
+L2:
     LDR($A) R0
     LDR($B) R1
     ALUOUT(%ADD) R2
@@ -10,13 +15,12 @@ L1:
     MOV R0 , R1
     MOV R1 , R2
 
+    OUT R1
+
     LDR($A) R1
-    LDR($B) 1000
+    LDR($B) 0x7000
     ALUTEST(%SUB)
 
-    JMP(#U) *DONE
+    JMP(#U) *FIBONACCI
 
-    JMP(#J) *L1
-
-DONE:
-    HLT
+    JMP(#J) *L2
