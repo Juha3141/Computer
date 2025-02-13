af4423bc009c2bd90b9165dc9f4fe0299afe8d4b
Ian Juha Cho <ianisnumber2027@gmail.com>
2025-02-13 12:13:41 +0900

    Update fibonacci.asm

--- assembler/fibonacci.asm
+++ assembler/fibonacci.asm
@@ -7,6 +7,8 @@ FIBONACCI:
     MOV R0 , 0x00
     MOV R1 , 0x01
     MOV R2 , 0x00
+
+    MOV R3 , 0x00
 L2:
     LDR($A) R0
     LDR($B) R1
@@ -16,11 +18,19 @@ L2:
     MOV R1 , R2
 
     OUT R1
+    
+    MOV *R3 , R1
+    LDR($A) R3
+    LDR($B) 1
+    ALUOUT($ADD) R3
 
     LDR($A) R1
     LDR($B) 0x7000
     ALUTEST(%SUB)
 
-    JMP(#U) *FIBONACCI
+    JMP(#U) *END
 
     JMP(#J) *L2
+
+END:
+    HLT
